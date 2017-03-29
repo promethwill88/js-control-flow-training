2c70f8d7aa42fd2d30dd96ed32de908dbe178556
promethwill88 <wfong.bk@gmail.com>
2017-03-28 23:30:01 -0700

    fixed beer deduction

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -1,16 +1,20 @@
 console.log("sing.js is loaded");
+
 let bottlesOfBeer = ['bottles of beer on the wall,', 'bottles of beer!', 'Take one down and pass it around,', 'bottles of beer on the wall!'
 ];
-let bottle = 5;
-
-for(let i = 0; i <= bottlesOfBeer.length; i++){
+let bottle;
+for(bottle = 20; bottle > 0; bottle--){
 	console.log(bottle + " " + bottlesOfBeer[0]);
 	console.log(bottle + " " + bottlesOfBeer[1]);
 	console.log(bottlesOfBeer[2]);
-	bottle--;
-	console.log(bottle + " " + bottlesOfBeer[3]);
-
-};
-if(bottle = 1){
-		bottlesOfBeer[3] = 'bottles of beer on the wall!';
+	console.log((bottle - 1) + " " + bottlesOfBeer[3]);
 	}
+
+/*	if(bottle < 1){
+		break;
+	}
+	else if(bottle = 1){
+		bottlesOfBeer[3] = "bottle of beer on the wall!";
+	}
+*/	
+//let verses = prompt('How many verses do you want to hear?');
