263bffb98760d45de43d80aadfdb5cc595b9e7d0
promethwill88 <wfong.bk@gmail.com>
2017-03-28 20:46:53 -0700

    added password validation

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,21 @@
 console.log("login.js is loaded");
+let userLogin = {username:'promethwill88', password:'password123'};
+let userPassword = prompt("Enter password for user promethwill88");
+if(userPassword === userLogin.password){
+	alert("Password Accepted - Welcome promethwill88");
+}
+else{
+	let userPasswordWrong = prompt("Incorrect Password (Attempt 2/3) - Please try again.");
+	if(userPasswordWrong === userLogin.password){
+		alert("Password Accepted - Welcome promethwill88");
+	}
+	else{
+		let userPasswordWrongFinal = prompt("Incorrect Password (Attempt 3/3) - Please try again.");
+		if(userPasswordWrongFinal === userLogin.password){
+			alert("Password Accepted - Welcome promethwill88");
+		}
+		else{
+		alert("Account Locked");
+		}
+	}
+}
