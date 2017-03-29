91daba48cb14081125e6ff1ea411acf9779da60d
promethwill88 <wfong.bk@gmail.com>
2017-03-28 21:26:45 -0700

    added bottles of beer song

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -1,1 +1,16 @@
 console.log("sing.js is loaded");
+let bottlesOfBeer = ['bottles of beer on the wall,', 'bottles of beer!', 'Take one down and pass it around,', 'bottles of beer on the wall!'
+];
+let bottle = 5;
+
+for(let i = 0; i <= bottlesOfBeer.length; i++){
+	console.log(bottle + " " + bottlesOfBeer[0]);
+	console.log(bottle + " " + bottlesOfBeer[1]);
+	console.log(bottlesOfBeer[2]);
+	bottle--;
+	console.log(bottle + " " + bottlesOfBeer[3]);
+
+};
+if(bottle = 1){
+		bottlesOfBeer[3] = 'bottles of beer on the wall!';
+	}
