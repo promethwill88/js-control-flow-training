c251a4e48e9aa72170d3a9e04d471107bdd37b6f
promethwill88 <wfong.bk@gmail.com>
2017-03-28 20:11:11 -0700

    created security question logic

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,17 @@
 console.log("security_questions.js is loaded");
+let securityQuestions = [
+{question: "Who was your childhood best friend?", expectedAnswer: "mark"}, 
+{question: "What was the name of your first pet?", expectedAnswer: "snoopy"},
+{question: "What was the brand of your first car?", expectedAnswer: "bmw"}
+];
+
+for(var i = 0; i < securityQuestions.length; i++){
+	var answer = prompt(securityQuestions[i].question);
+	var answerLower = answer.toLowerCase();
+	if(answerLower === securityQuestions[i].expectedAnswer){
+	}
+	else{
+	alert("Answer does not match");
+	break;
+	}
+};
